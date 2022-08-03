8afd9bbd9f03ff3773e51593925f3bee91a4dc37
jj131204 <jj131204@gmail.com>
2022-08-03 17:19:45 -0500

    estilo del codigo

--- lista_de_compras/bin/lista_de_compras.dart
+++ lista_de_compras/bin/lista_de_compras.dart
@@ -8,53 +8,43 @@ var list = [];
  */
 
 void listEdit() {
-
-
   print('¿deseas editar(1), elminiar(2) o agregar(3) un campo de la lista?');
   String? options = stdin.readLineSync();
 
   int options1 = int.parse(options!);
 
   if (options1 == 1) {
-
     print('¿Que Campo de la lista deseas editar (1,2,3,4....)?');
     String? campoList = stdin.readLineSync();
     int campoList1 = int.parse(campoList!);
 
-    
-
-    if(list.length + 1 < campoList1){
+    if (list.length + 1 < campoList1) {
       print('ingresa un numero valido');
       changueList();
-    }
-    else{
+    } else {
       int position = campoList1 - 1;
-    print('¿Por cual producto quieres cambiar?');
-    String? newProduct = stdin.readLineSync();
+      print('¿Por cual producto quieres cambiar?');
+      String? newProduct = stdin.readLineSync();
 
       list[position] = newProduct;
       print(' Su nueva lista de compras quedo de la siguiente manera: $list');
       changueList();
     }
-
   } else if (options1 == 2) {
-
     print('¿Que Campo de la lista deseas eliminar (1,2,3,4....)?');
     String? campoList = stdin.readLineSync();
     int campoList1 = int.parse(campoList!);
 
-    if(list.length + 1 < campoList1){
+    if (list.length + 1 < campoList1) {
       print('ingresa un numero valido');
       changueList();
-    }
-    else{
+    } else {
       int position = campoList1 - 1;
       //list.remove(position);
       list.removeAt(position);
       print(' Su nueva lista de compras quedo de la siguiente manera: $list');
       changueList();
     }
-
   } else if (options1 == 3) {
     print('¿Que producto deseas agregar?');
     String? addList = stdin.readLineSync();
@@ -68,22 +58,20 @@ void listEdit() {
   }
 }
 
-
 // Editar los campos de la lista.
-void changueList(){
-      print('¿Deseas editar tu lista de compras?');
-      String? editList = stdin.readLineSync();
+void changueList() {
+  print('¿Deseas editar tu lista de compras?');
+  String? editList = stdin.readLineSync();
 
-      if (editList == 'si') {
-        listEdit();
-      } else if (editList == 'no') {
-        print('Esta es su lista de compra: $list');
-        print('bye');
-
-      } else {
-        print('Ingrese un valor valido (si o no)');
-        changueList();
-      }
+  if (editList == 'si') {
+    listEdit();
+  } else if (editList == 'no') {
+    print('Esta es su lista de compra: $list');
+    print('bye');
+  } else {
+    print('Ingrese un valor valido (si o no)');
+    changueList();
+  }
 }
 
 /**
@@ -106,10 +94,8 @@ void addList(valor) {
 
       // Editar la lista de compras
       changueList();
-
     } else if (newProduct != 'si') {
       print('ingresa un valor  valido (Si o No)');
-      
     }
   }
 }
